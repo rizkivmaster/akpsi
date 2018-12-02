3519f156dd0070402aec0e583163986ae2a98c57
rr <rizkivmaster@gmail.com>
2018-12-02 14:56:55 +0700

    added use cases mock

--- app.py
+++ app.py
@@ -3,14 +3,15 @@ from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
-@app.route('/home', methods=['GET', 'POST'])
-def home():
+@app.route('/', defaults={'path': 'Dashboard.html'})
+@app.route('/<path:path>', methods=['GET', 'POST'])
+def home(path):
     error = None
     if request.method == 'POST':
         if request.form['username'] != 'admin' or request.form['password'] != 'admin':
             error = 'Invalid Credentials. Please try again.'
         else:
-            return render_template('dashboard.html', error=error)
-    return render_template('login.html', error=error)
+            return render_template('Dashboard.html', error=error)
+    return render_template(path, error=error)
 
-app.run(host='127.0.0.1', port= 5000)
+app.run(host='0.0.0.0', port= 5000)
